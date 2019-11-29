f33831567e277327e3b98a0a475f5ab317d5a309
Ruchi Sarkar <ruchisarkar1998@gmail.com>
2019-11-29 14:56:10 -0500

    fooling around w server

--- homeless-shelter/src/App.js
+++ homeless-shelter/src/App.js
@@ -1,52 +1,86 @@
 import React from 'react';
-import logo from './logo.svg';
 import './bulma.css';
+import './pictures/angel.png';
+import About from './about/About.js';
+import axios from 'axios';
+import { BrowserRouter as Link } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 
-function App() {
-  return (
-    <div className="App">
-    <section className="hero is-primary">
-    <div className="hero-body">
+class App extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      redirect: false
+    }
+    axios.post('http:localhost:3000/account.create', {
+      "name": "test",
+      "pass": "pass123",
+      "data": {
+        "phone": "phoneNum",
+        "address": "123 Durham"
+      }
+    }).then(function (response) {
+      console.log(response);
+    })
+    .catch(function (error) {
+      console.log(error);
+    });
+  }
+
+  setRedirect = () => {
+    this.setState({
+      redirect: true
+    });
+  }
+
+  renderRedirect = () => {
+    if(this.state.redirect) {
+      return(
+        <Switch>
+          <Route><About /></Route>
+        </Switch>
+      );
+    }
+  }
+
+  render() {
+    return(
+    <Router>
+      <div className="App">
+      {this.renderRedirect()}
+      <section className="hero is-primary">
+        <div className="hero-body">
         <div className="container">
-            <h1 className="title">
-                Guardian Angels
-            </h1>
-            <h2 className="subtitle">
-            No one gets left behind.
-            </h2>
-            <h3>Donate now and help those in need.</h3>
+          <h1 className="title">Guardian Angels</h1>
+          <h2 className="subtitle">No one gets left behind.</h2>
+          <h3>Donate now and help those in need.</h3>
         </div>
-    </div>
-</section>
-<section>
-<nav className="navbar is-dark" role="navigation" aria-label="main navigation">
+        </div>
+      </section>
+      <section>
+      <nav className="navbar is-dark" role="navigation" aria-label="main navigation">
         <div className="navbar-brand">
           <a className="navbar-item" href="https://bulma.io">
             <img src="./pictures/angel.png" width="25" height="50"></img>
           </a>
-      
           <a role="button" className="navbar-burger burger" aria-label="menu" aria-expanded="false" data-target="navbarBasicExample">
             <span aria-hidden="true"></span>
             <span aria-hidden="true"></span>
             <span aria-hidden="true"></span>
           </a>
         </div>
-      
         <div id="navbarBasicExample" className="navbar-menu">
           <div className="navbar-start">
             <a className="navbar-item">
               Home
             </a>
-      
-            <a className="navbar-item">
+            <a className="navbar-item" onClick={this.setRedirect}>
               About Us
             </a>
-      
             <div className="navbar-item has-dropdown is-hoverable">
               <a className="navbar-link">
                 Shelters
               </a>
-      
               <div className="navbar-dropdown">
                 <a className="navbar-item">
                   Near me
@@ -63,37 +97,33 @@ function App() {
               </div>
             </div>
           </div>
-      
           <div className="navbar-end">
             <div className="navbar-item">
               <div className="buttons">
                 <a id="signUp" className="button is-primary">
                   <strong>Sign up</strong>
                 </a>
-                <a className="button is-light">
-                  Log in
-                </a>
+                <a className="button is-light">Log in</a>
               </div>
             </div>
           </div>
         </div>
       </nav>
     </section> 
-<section className="section">
+    <section className="section">
     <div className="container">
         <div className="content">
-            <h1 className="title">
-                Our Mission:
-            </h1>
+            <h1 className="title">Our Mission:</h1>
             <h3 className="subtitle is-grey is-6">And our values.</h3>
             <p>Bacon ipsum dolor amet turducken kielbasa alcatra meatball jowl tenderloin buffalo pastrami short ribs chicken cow turkey. Turkey short loin meatball buffalo sausage boudin. Jerky short loin chuck ribeye pastrami shoulder, t-bone pig tail tenderloin shankle filet mignon picanha ham. Tongue ground round leberkas, tri-tip turducken beef rump ribeye meatloaf short ribs pancetta. Jowl strip steak porchetta, tri-tip spare ribs fatback beef kevin doner tenderloin sausage salami swine tail. Corned beef boudin shank drumstick burgdoggen cupim buffalo.
-
                     Jerky venison ball tip, tail beef ribs bresaola spare ribs. Ball tip buffalo leberkas, andouille salami filet mignon tri-tip jowl shoulder cow porchetta kevin short loin. Cow rump turkey andouille burgdoggen landjaeger picanha bresaola. Corned beef beef ribs beef, ground round pig jerky shankle swine ham chicken. Ball tip alcatra leberkas meatball pastrami, frankfurter ribeye. Pancetta beef shoulder beef ribs, turducken cupim cow ham hock short ribs.</p>
         </div>
     </div>
-</section>
-</div>
+    </section>
+    </div>
+    </Router>
   );
+    }
 }
 
 export default App;
