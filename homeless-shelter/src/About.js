2162a9c9b7d526ae8c82e88c836da0e3581c1486
Ruchi Sarkar <ruchisarkar1998@gmail.com>
2019-12-09 20:37:46 -0500

    homepage and about

--- homeless-shelter/src/About.js
+++ homeless-shelter/src/About.js
@@ -1,11 +1,47 @@
 import React from 'react';
 import MyNavbar from './myNavbar.js';
+import {Card, CardBody, CardTitle, CardText} from 'reactstrap';
+import './About.css';
 
 function About() {
     return (
       <div>
         <MyNavbar />
-        <h1>About us</h1>
+        <Card className="about">
+          <CardBody>
+            <CardTitle><h3>About Us</h3></CardTitle>
+            <CardText>
+              <h5>
+                Our mission is to provide homeless shelters with a more seamless intake process and the ability to easily 
+                articulate which donations are most needed at any given time.
+              </h5>
+              <p>
+                Currently, the majority of homeless shelter intake processes are done on paper. Furthermore, individuals check into 
+                shelters without a chance to articulate what items they need the most at that particular moment. Guardian Angels hopes to 
+                address both of these issues. <a href="/signup">Register</a> to become an affiliated shelter today and unlock features to 
+                check in individuals electronically, eliminating the potential for lost paper records. 
+              </p>
+              <p>
+                During intake, an individual checking in can specify which items they most urgently need
+                upon check-in. We compile the data across all the individuals staying in your shelter, and 
+                make real-time calculations to determine the items which are most needed across your shelter as a whole. 
+              </p>
+              <p>
+                All affiliated shelters will also be able to make announcements and posts on a private feed
+                accessible only to other shelters registered with Guardian Angels. 
+              </p>
+            </CardText>
+          </CardBody>
+        </Card>    
+        <Card className="about">
+          <CardBody>
+            <CardText>
+              <p>
+                Not affiliated with a shelter? Still want to contribute? <a href='./all'>Pledge</a> a donation to your favorite shelter! 
+              </p>
+            </CardText>
+          </CardBody>
+        </Card>    
       </div>
         
     );
--- homeless-shelter/src/App.js
+++ homeless-shelter/src/App.js
@@ -1,7 +1,8 @@
 import React from 'react';
 
 import MyNavbar from './myNavbar.js';
-
+import {Card, CardBody, CardTitle, CardText, Row, Col} from 'reactstrap';
+import './App.css';
 
 
 class App extends React.Component {
@@ -14,9 +15,35 @@ class App extends React.Component {
     return (
       <div>
         <MyNavbar />
-        <div>
-          <h1>Guardian Angels</h1>
-          <p></p>
+        <div id="home">
+          <Card className="card">
+            <CardBody>
+              <CardTitle>
+                <h3>Guardian Angels</h3>
+              </CardTitle>
+              <CardText>
+                <p>Committed to providing a smooth and seamless experience for homeless shelters and those they serve</p>
+              </CardText>
+            </CardBody>
+          </Card>
+          <Row>
+            <Col sm={6}>
+              <Card className="card">
+                <CardBody>
+                  <CardTitle><h4>Are you affiliated with a shelter?</h4></CardTitle>
+                  <CardText><p><a href="/signup">Sign up</a> or <a href="/login">login</a> today!</p></CardText>
+                </CardBody>
+              </Card >
+            </Col>
+            <Col sm={6}>
+              <Card className="card">
+                <CardBody>
+                <CardTitle><h4>Interested in donating?</h4></CardTitle>
+                <CardText><p>Make your <a href='/all'>pledge</a> today!</p></CardText>
+                </CardBody>
+              </Card>
+            </Col>
+          </Row>
         </div>
       </div>);
   }
